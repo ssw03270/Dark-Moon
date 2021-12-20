41bc29202e1810d482cd21a661e3afa42dc46453
SeungWon Seo <ttd8591@gmail.com>
2021-12-20 11:14:37 +0900

    add simple task all

---
+++ DarkMoon/Assets/Scripts/Card/SimpleTask/SimpleWeak.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleWeak : MonoBehaviour
+{
+    Field current_field = GameObject.Find("FieldManager").GetComponent<Field>();
+    public void AddWeak(int entity_position, int amount)
+    {
+        if (entity_position < 0 && entity_position >= current_field.enemy_entity.Length)
+        {
+            Debug.Assert(entity_position < 0 && entity_position >= current_field.enemy_entity.Length, "Wrong Entity Position");
+            return;
+        }
+        current_field.enemy_entity[entity_position].entity_weak += amount;
+    }
+}
