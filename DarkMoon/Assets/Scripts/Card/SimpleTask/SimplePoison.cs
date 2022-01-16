f3cc62f811a30e9992e40f2bc40893ee9d0fe4b1
SeungWon Seo <ttd8591@gmail.com>
2022-01-17 01:55:03 +0900

    fix simple task

--- DarkMoon/Assets/Scripts/Card/SimpleTask/SimplePoison.cs
+++ DarkMoon/Assets/Scripts/Card/SimpleTask/SimplePoison.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 
 public class SimplePoison : SimpleTask
 {
-<<<<<<< HEAD
     FieldManager current_field = GameObject.Find("FieldManager").GetComponent<FieldManager>();
     public override void Task(int entity_position, int amount)
-=======
-    Field current_field = GameObject.Find("FieldManager").GetComponent<Field>();
-    public void AddPoison(int entity_position_start, int entity_position_end, int amount)
->>>>>>> fd04b1025c0b7a23bfac64a01619feac52a99c76
     {
-        if (entity_position_start > entity_position_end || entity_position_start < 0 || entity_position_end >= current_field.entity_list.Length)
+        if (entity_position < 0 || entity_position >= current_field.enemy_entity.Length)
         {
             Debug.Assert(true, "Wrong Entity Position");
             return;
         }
-        for (int i = entity_position_start; i <= entity_position_end; i++)
-        {
-            current_field.entity_list[i].entity_poison += amount;
-        }
+        current_field.enemy_entity[entity_position].entity_poison += amount;
     }
 }
--- DarkMoon/Assets/Scripts/Card/SimpleTask/SimpleStrength.cs
+++ DarkMoon/Assets/Scripts/Card/SimpleTask/SimpleStrength.cs
@@ -2,19 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleStrength : MonoBehaviour
+public class SimpleStrength : SimpleTask
 {
-    Field current_field = GameObject.Find("FieldManager").GetComponent<Field>();
-    public void AddStrength(int entity_position_start, int entity_position_end, int amount)
+    FieldManager current_field = GameObject.Find("FieldManager").GetComponent<FieldManager>();
+    public override void Task(int entity_position, int amount)
     {
-        if (entity_position_start > entity_position_end || entity_position_start < 0 || entity_position_end >= current_field.entity_list.Length)
+        if (entity_position < 0 || entity_position >= current_field.our_entity.Length)
         {
             Debug.Assert(true, "Wrong Entity Position");
             return;
         }
-        for (int i = entity_position_start; i <= entity_position_end; i++)
-        {
-            current_field.entity_list[i].entity_strength += amount;
-        }
+        current_field.our_entity[entity_position].entity_strength += amount;
     }
 }
