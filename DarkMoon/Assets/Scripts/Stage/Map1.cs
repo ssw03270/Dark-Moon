16cd15e17c1d534138004d7798db0867da77e68b
CHERISH61111 <84861196+CHERISH61111@users.noreply.github.com>
2022-01-22 02:18:48 +0900

    stage -> map1,map2
    change color

--- DarkMoon/Assets/Scripts/Stage/Map1.cs
+++ DarkMoon/Assets/Scripts/Stage/Map1.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Map1 : MonoBehaviour
 {
-    public GameObject StageMenu = null;
+    public GameObject StageMenu = null;  // back을 눌렀을 때 되돌아갈 StageMenu UI
+    Button bt;
 
-    public void BtnBack(){
-        StageMenu.SetActive(true);
-        this.gameObject.SetActive(false);
+    public void BtnBack(){  // Btn_Back을 눌렀을 때 실행되는 함수
+
+        StageMenu.SetActive(true);   // StageMenu UI는 활성화
+        this.gameObject.SetActive(false);  // 현재 UI는 비활성화
+
+        GameObject.Find("StageMenu").GetComponent<StageMenu>().can_play_stage2 = true;
+
+        bt = GameObject.Find("StageMenu").GetComponent<StageMenu>().btn_st1;  // 스테이지 해금됐을 때 색 바뀌도록, 나중에 보스전 clear했을 때 위치로 이동
+        ColorBlock colorBlock = bt.colors;
+        colorBlock.normalColor = new Color(1f,1f,1f);
+        bt.colors = colorBlock;
     }
 }
--- DarkMoon/Assets/Scripts/Stage/StageMenu.cs
+++ DarkMoon/Assets/Scripts/Stage/StageMenu.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StageMenu : MonoBehaviour
 {
-    public GameObject goMap1 = null;
-    public bool can_play_stage1 = true;
+    public GameObject goMap1 = null;  // Btn_St1 을 클릭했을 때 이동해야할 UI를 오브젝트로 받음
+    public GameObject goMap2 = null;
+    public Button btn_st1;
+    public bool can_play_stage1 = true;  // stage1을 플레이할 수 있는 상태인지 체크할 수 있는 bool 변수
+                                            //(다른 stage의 경우, 이전 stage의 보스전을 클리어 했을 때 true가 되도록 할 예정)
+    public bool can_play_stage2 = false;
 
-    public void BtnSt1(){
 
-        if(can_play_stage1){
-            goMap1.SetActive(true);
-            this.gameObject.SetActive(false);   
+    public void BtnSt1(){   // Btn_St1을 클릭했을 때 실행되는 함수
+
+        if(can_play_stage1){   // 해금이 된 스테이지일 경우
+            goMap1.SetActive(true);  // 해당 map UI를 활성화 하고 
+            this.gameObject.SetActive(false);   // 현재 UI는 비활성화
         }
-        
     }
+    public void BtnSt2(){   // Btn_St1을 클릭했을 때 실행되는 함수
+
+        if(can_play_stage2){   // 해금이 된 스테이지일 경우
+            goMap2.SetActive(true);  // 해당 map UI를 활성화 하고 
+            this.gameObject.SetActive(false);   // 현재 UI는 비활성화
+        }
+    }
+
+    
 }
