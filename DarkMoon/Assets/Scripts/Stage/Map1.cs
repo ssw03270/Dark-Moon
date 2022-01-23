b31149df2ea7ce737e8be4e96a6a939716237319
CHERISH61111 <84861196+CHERISH61111@users.noreply.github.com>
2022-01-24 00:23:47 +0900

    locked text

--- DarkMoon/Assets/Scripts/Stage/Map1.cs
+++ DarkMoon/Assets/Scripts/Stage/Map1.cs
@@ -13,11 +13,13 @@ public class Map1 : MonoBehaviour
         StageMenu.SetActive(true);   // StageMenu UI는 활성화
         this.gameObject.SetActive(false);  // 현재 UI는 비활성화
 
+        // 스테이지 해금
         GameObject.Find("StageMenu").GetComponent<StageMenu>().can_play_stage2 = true;
 
-        bt = GameObject.Find("StageMenu").GetComponent<StageMenu>().btn_st1;  // 스테이지 해금됐을 때 색 바뀌도록, 나중에 보스전 clear했을 때 위치로 이동
+        bt = GameObject.Find("StageMenu").GetComponent<StageMenu>().btn_st2;  // 스테이지 해금됐을 때 색 바뀌도록, 나중에 보스전 clear했을 때 위치로 이동
         ColorBlock colorBlock = bt.colors;
-        colorBlock.normalColor = new Color(1f,1f,1f);
+        colorBlock.normalColor = new Color(1f,1f,1f); 
         bt.colors = colorBlock;
+
     }
 }
--- DarkMoon/Assets/Scripts/Stage/StageMenu.cs
+++ DarkMoon/Assets/Scripts/Stage/StageMenu.cs
@@ -5,26 +5,39 @@ using UnityEngine.UI;
 
 public class StageMenu : MonoBehaviour
 {
-    public GameObject goMap1 = null;  // Btn_St1 을 클릭했을 때 이동해야할 UI를 오브젝트로 받음
-    public GameObject goMap2 = null;
-    public Button btn_st1;
+    public GameObject GoMap1 = null;  // Btn_St1 을 클릭했을 때 이동해야할 UI를 오브젝트로 받음
+    public GameObject GoMap2 = null;
+    public GameObject PrefabLockedText; // 해금되지 않은 stage 클릭 시 띄워지는 text의 prefab
+    public Button btn_st2;
+    public Button btn_st3;
+
     public bool can_play_stage1 = true;  // stage1을 플레이할 수 있는 상태인지 체크할 수 있는 bool 변수
                                             //(다른 stage의 경우, 이전 stage의 보스전을 클리어 했을 때 true가 되도록 할 예정)
     public bool can_play_stage2 = false;
-
+    public bool can_play_stage3 = false;
+    public bool can_play_stage4 = false;
+    public bool can_play_stage5 = false;
+    public bool can_play_stage6 = false;
+    public bool can_play_stage7 = false;
 
     public void BtnSt1(){   // Btn_St1을 클릭했을 때 실행되는 함수
 
         if(can_play_stage1){   // 해금이 된 스테이지일 경우
-            goMap1.SetActive(true);  // 해당 map UI를 활성화 하고 
+            GoMap1.SetActive(true);  // 해당 map UI를 활성화 하고 
             this.gameObject.SetActive(false);   // 현재 UI는 비활성화
         }
+        
     }
     public void BtnSt2(){   // Btn_St1을 클릭했을 때 실행되는 함수
 
         if(can_play_stage2){   // 해금이 된 스테이지일 경우
-            goMap2.SetActive(true);  // 해당 map UI를 활성화 하고 
+            GoMap2.SetActive(true);  // 해당 map UI를 활성화 하고 
             this.gameObject.SetActive(false);   // 현재 UI는 비활성화
+        }
+        else{ // 잠겨 있는 stage일 경우 'unlocked' 문구를 표시
+
+            var clone = Instantiate(PrefabLockedText, btn_st2.transform.position + new Vector3(0 ,40 ,0), Quaternion.Euler(Vector3.zero)); // prefeb 객체 생성
+            clone.transform.SetParent(this.transform);  // 현재 UI를 부모로 두도록 함
         }
     }
 
