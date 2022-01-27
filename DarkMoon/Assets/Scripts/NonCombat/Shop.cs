7b91a5ae7b26aa042c71264e35fdc29d4f54f4aa
Moon SeokHo <msh0614@khu.ac.kr>
2022-01-28 00:39:21 +0900

    update shop.cs (add ShopDisplay, Buy)
    update shop.cs
    add ShopDisplay function,
    add Buy function

--- DarkMoon/Assets/Scripts/NonCombat/Shop.cs
+++ DarkMoon/Assets/Scripts/NonCombat/Shop.cs
@@ -1,27 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Shop : MonoBehaviour
 {
-    public RectTransform shop_ui;
-    public TempManager temp_manager;
+    public RectTransform shop_ui; // 상점 UI
+    public RectTransform shop_slots; // 상점 슬롯 부모
+    public TempManager temp_manager; // 골드 관리용 게임매니저(임시)
+
+    public List<TempCard> shop_cards = new List<TempCard>(); // 상점에 진열할 카드 목록
+
+    private void Awake()
+    {
+        ShopDisplay();
+    }
 
     public void ShopEnterButton()
     {
-        shop_ui.anchoredPosition = Vector3.zero;
+        shop_ui.anchoredPosition = Vector3.zero; // 상점 위치로 갖다 놓기
     }
 
     public void ShopExitButton()
     {
-        shop_ui.anchoredPosition = Vector3.up * 1000;
+        shop_ui.anchoredPosition = Vector3.up * 1000; // 안 보이게 갖다 놓기
     }
 
-    public void Buy(int price)
+    private void ShopDisplay()
+    // 상점에 카드 진열 함수
     {
+        if (shop_slots.childCount == shop_cards.Count)
+        {
+            for (int i = 0; i < shop_cards.Count; ++i)
+            {
+                shop_slots.GetChild(i).GetComponent<TempCardDisplay>().card = shop_cards[i];
+            }
+        }
+    }
+
+    public void Buy()
+    {
+        int price = EventSystem.current.currentSelectedGameObject.GetComponent<TempCardDisplay>().card.card_price;
         if (temp_manager.gold_amount >= price)
         {
-            
+            Debug.Log(price);
+            temp_manager.gold_amount -= price;
+            temp_manager.GoldUpdate();
+
+            // 구매한 아이템 등록 처리
+        }
+        else
+        {
+            Debug.Log("not enough money");
         }
         
     }
--- DarkMoon/Assets/Scripts/NonCombat/TempManager.cs
+++ DarkMoon/Assets/Scripts/NonCombat/TempManager.cs
@@ -8,16 +8,14 @@ public class TempManager : MonoBehaviour
     public int gold_amount;
     public TextMeshProUGUI gold_text;
 
-    //public List<TempCard> card_data = new List<TempCard>(); // 임시용
-
     void Start()
     {
-
+        GoldUpdate();
     }
 
     void Update()
     {
-        //GoldUpdate();
+
     }
 
     public void GoldUpdate()
